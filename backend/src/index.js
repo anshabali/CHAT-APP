ed25942d17dc0eefb754c02cc4b3194e49de1d76
anshabali <anshabali786@gmail.com>
2025-03-07 02:27:11 +0530

    added file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
@@ -11,6 +14,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
