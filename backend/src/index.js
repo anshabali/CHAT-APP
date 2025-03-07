7f3fb31e0c5e03ffe1458a789987e4ade99057b6
anshabali <anshabali786@gmail.com>
2025-03-07 18:28:40 +0530

    added backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(5001, () => {
   console.log("server is running on PORT:" + PORT);
